0f2fcb5e2af3f0ccc8d20f7dccd95e8285a2b9c2
OpchetZ <varanyookasamsuk@gmail.com>
2024-07-04 15:26:27 +0700

    week3

---
+++ screens/week3/Resort.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView,  View } from 'react-native';
+
+export default function Resort() {
+    return (
+        <ScrollView>
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                               
+            </View>
+        </ScrollView>
+    );
+}
